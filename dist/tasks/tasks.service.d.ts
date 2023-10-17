df8688f68bcea426fda68cfa27db4ab1ba0b0242
daniyalatbeam <ahmed.daniyal@beam.so>
2023-10-17 19:34:51 +0500

    task management CRUD with local data

---
+++ dist/tasks/tasks.service.d.ts
@@ -0,0 +1,10 @@
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+export declare class TasksService {
+    private tasks;
+    getAllTasks(): Task[];
+    createTask(CreateTaskDto: CreateTaskDto): Task;
+    getTaskById(id: string): Task;
+    deleteTask(id: string): void;
+    updateTaskStatus(id: string, status: TaskStatus): Task;
+}
